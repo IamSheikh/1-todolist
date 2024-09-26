0296b767f939a64752eda4d27001f9c32485df5e
babydmg <126503300+babydmg@users.noreply.github.com>
2024-09-26 16:38:30 +0500

    Final stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, FormEvent, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Task {
+  id: number;
+  task: string;
+  isCompleted: boolean;
 }
 
-export default App
+const App = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleForm = (e: FormEvent) => {
+    e.preventDefault();
+    const newTask = {
+      task,
+      id: tasks?.length ? (tasks[tasks.length - 1].id ?? 0) + 1 : 1,
+      isCompleted: false,
+    };
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    setTask("");
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col mt-4">
+      <h1 className="text-4xl font-bold">
+        <span className="text-purple-500">Allison Burgers</span> Todo List
+      </h1>
+      <form onSubmit={handleForm} className="mt-4">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add new task"
+          className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          required
+        />
+
+        <button
+          type="submit"
+          className=" bg-blue-500
+          hover:bg-blue-600
+          text-white
+          font-semibold
+          py-2
+          px-4
+          ml-2
+          rounded-lg
+          focus:outline-none
+          focus:ring-2
+          focus:ring-blue-500
+          focus:ring-offset-2"
+        >
+          Add
+        </button>
+      </form>
+
+      <ul className="space-y-4 mt-2">
+        {tasks.map((t) => {
+          return (
+            <li className="flex items-center justify-between p-3 flex-grow bg-gray-50 rounded-md shadow w-96">
+              <div className="flex items-center flex-wrap">
+                <input
+                  type="checkbox"
+                  className={`h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded`}
+                  checked={t.isCompleted}
+                  onChange={(e) => {
+                    const currentTask = tasks.findIndex((tk) => tk.id === t.id);
+                    const updatedTasks = [...tasks];
+                    updatedTasks[currentTask].isCompleted = e.target.checked;
+                    setTasks(updatedTasks);
+                  }}
+                />
+                <span
+                  className={`ml-3 text-gray-800 text-base break-words ${
+                    t.isCompleted ? "line-through text-gray-400" : ""
+                  }`}
+                >
+                  {t.task}
+                </span>
+              </div>
+              <div className="flex space-x-2 items-center">
+                <button
+                  className="bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-2 rounded-md text-xs"
+                  onClick={() => {
+                    const currentTask = tasks.findIndex((tk) => tk.id === t.id);
+                    const newPrompt = prompt("Edit task", t.task);
+                    if (newPrompt !== "") {
+                      const updatedTasks = [...tasks];
+                      updatedTasks[currentTask].task = newPrompt ?? "";
+                      setTasks(updatedTasks);
+                    } else {
+                      alert("Task cannot be empty");
+                    }
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  className="bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded-md text-xs"
+                  onClick={() => {
+                    const currentTask = tasks.findIndex((tk) => tk.id === t.id);
+                    const updatedTasks = [...tasks];
+                    updatedTasks.splice(currentTask, 1);
+                    setTasks(updatedTasks);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
